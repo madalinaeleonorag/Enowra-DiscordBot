95869f45cc8bd645a33880acfcbc53b2b4bee00d
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2021-04-20 21:44:06 +0300

    started the bot with !ping pong command by default

---
+++ bot.js
@@ -0,0 +1,41 @@
+var Discord = require("discord.io");
+var logger = require("winston");
+var auth = require("./auth.json");
+
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console(), {
+  colorize: true,
+});
+logger.level = "debug";
+
+var bot = new Discord.Client({
+  token: auth.token,
+  autorun: true,
+});
+
+bot.on("ready", function (evt) {
+  logger.info("Connected");
+  logger.info("Logged in as: ");
+  logger.info(bot.username);
+});
+
+bot.on("message", function (user, userID, channelID, message, evt) {
+
+  if (message.substring(0, 1) == "!") {
+
+    var args = message.substring(1).split(" ");
+    var cmd = args[0];
+    args = args.splice(1);
+
+    switch (cmd) {
+      // !ping
+      case "ping":
+        bot.sendMessage({
+          to: channelID,
+          message: "Pong!",
+        });
+        break;
+      // Just add any case commands if you want to..
+    }
+  }
+});

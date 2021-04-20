f6d4769100a39d433b0276dfbf69916ceb6e640f
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2021-04-20 23:13:36 +0300

    replit hosting added

--- bot.js
+++ bot.js
@@ -1,3 +1,13 @@
+// REPLIT.COM localhost hosting
+// https://replit.com/@madalinaeleonor/Enowra-DiscordBot
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
+
 var Discord = require("discord.io");
 var logger = require("winston");
 var auth = require("./auth.json");

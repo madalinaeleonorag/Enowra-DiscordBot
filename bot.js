8e6e78523ccfe058763097ee8872856075d57127
Madalina-Eleonora Gheorghe <madalinaeleonora.gheorghe@gmail.com>
2022-10-26 14:24:21 +0300

    updated config auth

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
-// REPLIT.COM localhost hosting
-// https://replit.com/@madalinaeleonor/Enowra-DiscordBot
 const express = require("express");
+const config = require("./config.json");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => res.send("Hello World!"));
@@ -10,7 +9,6 @@ app.listen(port, () =>
 
 var Discord = require("discord.io");
 var logger = require("winston");
-var auth = require("./auth.json");
 
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console(), {
@@ -19,7 +17,7 @@ logger.add(new logger.transports.Console(), {
 logger.level = "debug";
 
 var bot = new Discord.Client({
-  token: auth.token,
+  token: config.BOT_TOKEN,
   autorun: true,
 });
 
